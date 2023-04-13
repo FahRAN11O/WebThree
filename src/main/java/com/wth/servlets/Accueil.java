d598af6a993694832e36f18ca0b242f58b475d3b
undefined <famenofa@gmail.com>
2023-04-13 09:38:24 +0300

    commit d'initiation du projet webthree

---
+++ src/main/java/com/wth/servlets/Accueil.java
@@ -0,0 +1,18 @@
+package com.wth.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Accueil extends HttpServlet {
+	
+	private final String URL_ACCUEIL = "/index.html";
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.getServletContext().getRequestDispatcher(URL_ACCUEIL).forward(req, resp);
+	}
+}
